5351810f19f4d61aeb03fbb31e97f9af43d0b40f
yountek14 <benjita1b4@gmail.com>
2025-05-19 14:11:28 -0400

    Evaluacion completa Benjamin_Aguero

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,37 +1,31 @@
+arancel = 1800000
+valor_matricula = 90000
+descuento_arancel = 0
+descuento_matricula = 0
+
 promedio_final = float(input("Ingresa el promedio final con el que saliste del colegio o el liceo: "))
 print("Ingresa tu nivel socioeconomico, donde 1 representa mayor vulnerabilidad y 5 menor vulnerabilidad: ")
-quintil_socioeconomico = int(input("1-5: "))
+quintil = int(input("1-5: "))
 
-arancel_final = 1800000
-valor_matricula = 90000
-descuento_arancel = 0.0
-descuento_matricula = 0.0
 
-if promedio_final == 6.0 and (quintil_socioeconomico == 1 or quintil_socioeconomico == 2):
-    descuento_arancel = 0.2
 
-elif promedio_final == 6.0 and (quintil_socioeconomico == 3 or quintil_socioeconomico == 4):
-    descuento_arancel = 0.15
-
-elif 5.0 < promedio_final <= 6.0 and (quintil_socioeconomico == 1 or quintil_socioeconomico == 2):
-    descuento_arancel = 0.13
-
-elif 5.0 < promedio_final <= 6.0 and (quintil_socioeconomico == 3 or quintil_socioeconomico == 4):
-    descuento_arancel = 0.1
-
+if promedio_final > 6.0 and (quintil == 1 or quintil == 2):
+    descuento_arancel = arancel - (arancel * 0.2)
+elif promedio_final > 6.0 and (quintil == 3 or quintil == 4):
+    descuento_arancel = arancel - (arancel * 0.15)
+elif(promedio_final > 5.0 and promedio_final <= 6.0) and (quintil == 1 or quintil == 2):
+    descuento_arancel = arancel - (arancel * 0.13)
+elif(promedio_final > 5.0 and promedio_final <= 6.0) and (quintil == 3 or quintil == 4):
+    descuento_arancel = arancel - (arancel * 0.1)
 else:
-    descuento_arancel = 0
+    descuento_arancel = 180000
 
-if promedio_final >= 5.5 and (quintil_socioeconomico == 1 or  quintil_socioeconomico == 2 or quintil_socioeconomico == 3):
-    descuento_matricula = 0.1
-elif promedio_final > 5.5 and (quintil_socioeconomico == 1 or quintil_socioeconomico == 2 or quintil_socioeconomico == 3):
-    descuento_matricula = 0.15
+if (quintil == 1 or quintil == 2 or quintil == 3) and promedio_final >= 5.5:
+    descuento_matricula = valor_matricula - (valor_matricula * 0.15)
+elif quintil == 1 or quintil == 2 or quintil == 3:
+    descuento_matricula = valor_matricula - (valor_matricula * 0.1)
 else:
-    descuento_matricula = 0
+    descuento_matricula = valor_matricula
 
-valor_a = arancel_final - (arancel_final * descuento_arancel)
-
-valor_m = valor_matricula - (valor_matricula * descuento_matricula)
-
-print("El valor final del arancel es: ", valor_a)
-print("El valor final de la Matricula es: ", valor_m)
+print("El valor final del arancel es: ", descuento_arancel)
+print("El valor final de la Matricula es: ", descuento_matricula)
