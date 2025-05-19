3805fe1d2130604b08f4ec46fea795a17b6f2adf
amadorsin <acacaok@gmail.com>
2025-05-19 12:37:48 -0400

    Evaluación completa -Amador Suarez

--- ejercicio2.py
+++ ejercicio2.py
@@ -10,13 +10,32 @@ n2 = int(input("Ingresa el segundo número (el número mayor)\n"))
 nran = random.randint(n1,n2)
 
 int1 = int(input("Intenta adivinar\n"))
-if int1 < nran:
-    print("El número es mayor")
-elif int1 > nran:
-    print("El número es menor")
+if int1 != nran:
+    if int1 < nran:
+        print("El número es mayor")
+    elif int1 > nran:
+        print("El número es menor")
 
-int2 = int(input("Intenta de nuevo\n"))
-if int2 < nran:
-    print("El número es mayor")
-elif int2 > nran:
-    print("El número es menor")
+    int2 = int(input("Intenta de nuevo\n"))
+    if int2 != nran:
+        if int2 < nran:
+            print("El número es mayor")
+        elif int2 > nran:
+            print("El número es menor")
+        ab1 = abs(int1 - nran)
+        ab2 = abs(int2 - nran)
+        if ab1 < ab2:
+            print(f"El número más cercano es: {int1}")
+        else:
+            print(f"El número más cercano es: {int2}")
+        
+        int3 = int(input("Intenta la última vez\n"))
+        if int3 != nran:
+            print("Perdiste")
+            print(f"El número es: {nran}")
+        else:
+            print("Haz adivinado")
+    else:
+        print("Haz adivinado")
+else:
+    print("Haz adivinado")
