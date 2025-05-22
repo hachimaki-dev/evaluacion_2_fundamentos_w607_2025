3e99a71cc9088e7b548c8465573c0152d356e9d6
Alan-dotcom23 <bastianmv123@gmail.com>
2025-05-22 16:52:23 -0400

    evaluacion/alan_mancilla_valderas

--- ejercicio2.py
+++ ejercicio2.py
@@ -7,24 +7,46 @@ if limite_inferior > limite_superior:
     print("El limite inferior no puede ser mayor que el superior.")
 else:
     num_al = random.randint(limite_inferior, limite_superior)
-    num_ingresado = int(input("Adivine el numero: \n"))
+    num_ingresado_1 = int(input("Adivine el numero: \n"))
     
     intentos = 3
-    
-    while intentos > 0:
-        if num_ingresado < num_al:
-            print("El numero es mayor ")
-            intentos -= 1
-            print(f"Tienes {intentos} intentos")
-        elif num_ingresado > num_al:
-            print("El numero es menor ")
-            intentos -= 1
-            print(f"Tienes {intentos} intentos")
-        else:
-            print("Has adivinado el numero!")
-            break
-        num_ingresado = int(input("Intente de nuevo: \n"))
 
-        if intentos == 0:
-            print(f"El numero era {num_al}, Has perdido")
+    if num_ingresado_1 == num_al:
+            print("Has ganado!")
+    else:
+        intentos -= 1
+        if num_ingresado_1 > num_al:
+            print("El numero es menor ")
+        elif num_ingresado_1 < num_al:
+            print("El numero es mayor")
+            print(f"Tienes {intentos} intentos")
+
+        num_ingresado_2 = int(input("Intente de nuevo: \n"))
+        
+        if num_ingresado_2 == num_al:
+            print("Has ganado!")
+        else:
+            intentos -= 1
+            if num_ingresado_2 > num_al:
+                print("El numero es menor ")
+            elif num_ingresado_2 < num_al:
+                print("El numero es mayor")
+
+            dif1 = num_ingresado_1 - num_al if num_ingresado_1 > num_al else num_al - num_ingresado_1
+            dif2 = num_ingresado_2 - num_al if num_ingresado_2 > num_al else num_al - num_ingresado_2
+
+            if dif1 < dif2:
+                print("Pista: El primer numero que ingresaste esta mas cerca del numero a adivinar")
+            elif dif1 > dif2:
+                print("Pista: El segundo numero que ingresaste esta mas cerca del numero a adivinar")
+            else:
+                print("Ambos estuvieron igual de cerca")
+                print(f"Te quedan {intentos} intentos")
+
+            num_ingresado_3 = int(input("Ultimo intento: \n"))
+
+            if num_ingresado_3 == num_al:
+                print("Has adivinado el numero!")
+            else:
+                print(f"El numero era {num_al}, Has perdido")
         
