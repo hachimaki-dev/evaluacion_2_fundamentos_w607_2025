0f7f46be093d5a3e6217674ed4e95c2996d5b111
K4USTIC <cesarmolinaaraya31@gmail.com>
2025-06-04 11:11:57 -0400

    ejercicio 1 de cesar

---
+++ prueba3.py
@@ -0,0 +1,29 @@
+while True:
+    try:
+        conteo_personas = int(input("¿a cuantas personas desea registrar? "))
+        break
+    except:
+        print("el numero de personas debe ser entero.")
+
+    for persona in range(conteo_personas):
+        while True:
+            try:
+                dosis = int(input("¿cuantas dosis a recibido?"))
+                break
+            except ValueError:
+                print("el numero de dosis debe ser entero.")
+
+completo = 0
+incompleto = 0
+
+if dosis <= 3:
+    print("esquema completo.")
+    completo += 1
+else:
+    print("esquema incompleto.")
+    incompleto += 1
+
+print("numero de personas registradas con esquema completo:" , completo)
+print("numero de personas con esquema incompleto" , incompleto)
+
+
