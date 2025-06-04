b57b51380c5cd9151304843677543b9cec5bede3
cinnamonjvt <ja.mansillac@duocuc.cl>
2025-06-04 11:10:43 -0400

    ejercicio 1 javi

---
+++ vacunacion.py
@@ -0,0 +1,6 @@
+while True:
+    try:
+        totaldegente = int(input("ingrese la cantidad de gente a registrar: "))
+        if totaldegente > 0
+
+        int(input("¿cuantas dosis se han aplicado?"))
