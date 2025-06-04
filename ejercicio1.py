b327caab9389e3d6388e52b3cd129a44e57bfe9a
axel <natandreik11@gmail.com>
2025-06-04 11:11:00 -0400

    ejercicio 1 de Axel

---
+++ ejercicio1.py
@@ -0,0 +1,18 @@
+
+
+while True:
+    try:
+        perso_registrar = int(input("cuantas personas se van a registrar: "))
+        break  # Sale del bucle si no hay error
+    except:
+        print("Error, intente de nuevo")
+
+num = []
+
+dosis = int(input("cuantas dosis resibiste:"))
+dosis.added(num)
+
+print(num)
+
+#Si el usuario ingresa algo que no sea un número entero, mostrar el mensaje: `"Debe ingresar un número entero."
+
