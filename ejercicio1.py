ec775c91eb8e94dc75562884faf2a458221a30e0
Luiz <kitt.man.2002@gmail.com>
2025-06-04 11:11:47 -0400

    evalua

---
+++ ejercicio1.py
@@ -0,0 +1,28 @@
+print("Sistema verficacion de vacunas")
+
+def pedir_entero(mensaje):
+    whil True:
+        try:
+            valor = int(input(mensaje))
+            if valor < 0:
+                print("ingrese un numero entero")
+                return valor
+            else:
+    :
+            print("ingrese un numero entero")
+            
+            
+cantidad = pedir_entero("Ingrese la cantidad de vacunados")
+
+for i in range (1,cantidad +1):
+    print(f"peronas {i}:")
+dosis = pedir_entero("Ingrese la cantidad de personas registradas")  
+
+if dosis <=3:
+    print("Esquema completo")
+else:
+    print("Esquema incompleto")
+
+
+
+
