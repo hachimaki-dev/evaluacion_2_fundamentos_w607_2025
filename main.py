0fa8f97fc36e85a8d5c5a19858724ac266fcdd54
Lunalker <deinerquero1@gmail.com>
2025-06-12 11:01:20 -0400

    Es un momaso

--- main.py
+++ main.py
@@ -6,6 +6,37 @@ while True:
         numero_entero =int(input())
         if numero_entero > 5:
             print("el numero es mayor a cinco")
+            print("No es "link""
+            " es "zelda" "
+            "No es "cállate" "
+            "es "kyc" "
+            "No es "Que""
+            " es "k" "
+            "No es "meme" "
+            "es "Momo" "
+            "No es "wey/parce/etc""
+            " es "Lince""
+            " No es "Chica""
+            " es "Elfa
+            " No es "genial""
+            " es "shidori""
+            " No es "por favor""
+            " es "plox""
+            " No es "Falso""
+            " es "Feik""
+            " No es "corazón" "
+            "es "kora" "
+            "No es "sí""
+            " es "cy""
+            " No es "serio""
+            " es "sergio""
+            " No es "laik""
+            " es "naik" "
+            "No es "imbécil""
+            " es "imbecilo""
+            " No es "la verdad""
+            " es "la dvd""
+            " No Es Ser Raro, Es Ser único:(")
         break
     except ValueError:
         print("Intenelo nuevamente")
