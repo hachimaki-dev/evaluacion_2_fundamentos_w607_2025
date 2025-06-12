94191d33798dc6f5ec135f103bd1d12dd46b4835
BlackCatVSC <luc.garridos@duocuc.cl>
2025-06-12 10:52:27 -0400

    Que paso?

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ print("Ingrese un numero entero")
 while True:
     try:
         numero_entero =int(input())
+        if numero_entero > 5:
+            print("El numero es mayor a 5")
         break
     except ValueError:
         print("Intenelo nuevamente")
+      
