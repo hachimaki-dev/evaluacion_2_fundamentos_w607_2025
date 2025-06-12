48f5d9be8fc6fca26eef0d007ef127c179dd3496
benjvv04 <benjamontanaa@gmail.com>
2025-06-12 11:04:40 -0400

    ola

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-print("Ingrese un numero entero")
+def menu():
+
+    print("MENU PRINCIPAL")
+    print("1.- Verificar Numero")
+    print("2.- Quitar")
 
 while True:
-    try:
-        numero_entero =int(input())
-        if numero_entero > 5:
-            print("el numero es mayor a cinco")
+    menu()
+
+    opc = int(input("Ingrese una opcion"))
+
+    if opc == 1:
+        print("Ingrese numero")
+        while True:
+            try:
+                numero_entero =int(input())
+                if numero_entero > 5:
+                    print("el numero es mayor a cinco")
+                    break
+                elif numero_entero <= 5:
+                    print("el numero es igual o menor a cinco")
+                    break
+            except ValueError:
+                print("Intentelo nuevamente")
+    elif opc == 2:
+        print("Gracias por usar la aplicacion.")
         break
-    except ValueError:
-        print("Intenelo nuevamente")
-if numero_entero >= 5:
-    print("El numero es mayor o igual a 5")
-else:
-    print("El numero es menor a 5")
+    else:
+        print("Opcion Invalida")
+
