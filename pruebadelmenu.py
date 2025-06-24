7f7a60eae12ab9432122cd72c9d46b824fc16702
cinnnamonjvt <ja.mansillac@duocuc.cl>
2025-06-24 00:44:11 -0400

    cinamon.jcm

--- pruebadelmenu.py
+++ pruebadelmenu.py
@@ -1,18 +1,46 @@
+numeros= []
+def menu():
+    print("1.-ingresar n")
+    print("2.-mostar mayor")
+    print("3.-mostrar pedido")
+    print("4.-salir")
 
 
-def mostrar_menu():
-    while True
-    print("ingresar numero")
-    print("ingresar mayor")
-    print("ingresar menor")
-    print("terminar programa")
+while True:
+    menu()
+    try:
+        opcion=int(input("ingrese una opcion: "))
+    except ValueError:
+        print("debe ingresar una opcion valida")
+        continue
 
-
-
-
-    
-
-    def ingresar_numero(numeros):
+    if opcion==1:
         while True:
-                numero= input("ingrese numero entre 0 y 100.")
-                
+            try:
+                numero=int(input("ingrese un numero: "))
+                if 0 <= numero<=100:
+                    numeros.append(numero)
+                    print("numero agregado correctamente")
+                    break
+                else:
+                    print("debe ingresar un numero entre 0 y 100")
+            except ValueError:
+                print ("debes ingresar un numero enteeeroo")
+
+    elif opcion == 2:
+        if numero:
+            print("hasta ahora el mayor es: ", max(numeros))
+        else:
+            print("no se guardo n")
+
+    elif opcion == 3:
+        if numero:
+            promedio = sum(numeros) / len(numeros)
+            print (f"el promdio d los n ingresados es: {promedio:.2f}")
+        else:
+          print("no se han ingresado numeros")
+    elif opcion ==4:
+        print("fin del programa xaao")
+        break
+    else:
+        print("debe elegir una opcion valida")
--- vacunacion.py
+++ vacunacion.py
@@ -1,11 +1,25 @@
+esquema_completo=0
+esquema_incompleto=0
 while True:
-        totaldegente = int(input("ingrese la cantidad de gente a registrar: "))
-        if totaldegente > 0:
-
-        input("¿cuantas dosis se han aplicado?")
-        int
-
-        esquema_completo=3
-        esquema_en_proceso= (0,1,2,3)
-        esquema_incompleto=0
-        contador_dosis=
+    try:
+        cantidad_personas= int(input("ingrese la cantidad de gente a registrar: "))
+        break 
+    except ValueError: 
+        print("debe poner un n enterro")
+        
+for i in range (cantidad_personas):
+    while True:
+        try:
+            dosis= int(input("ingrese cantidad de dosis q recibio: "))
+            if dosis >=3:
+                 print("toas las vakunas")
+                 esquema_completo+=1 
+            else: 
+                 print("no todas las vacunas")
+                 esquema_incompleto+=1 
+            break 
+        except ValueError:
+                 print("debe ingresar un n entero") 
+  
+print(f"se registraron {esquema_completo} personas con esquema completo")
+print(f"se registraron {esquema_incompleto} persona con esquema incompleto")
