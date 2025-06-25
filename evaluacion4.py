64e766f7cce1cf27633c594de476ea2acbf9f802
Luiz <kitt.man.2002@gmail.com>
2025-06-25 11:14:57 -0400

    Avanze de la clase

---
+++ evaluacion4.py
@@ -0,0 +1,66 @@
+lista_usuario = []
+def Ingresar_usuario(usuario,nombre,sexo,contraseña):
+    lista_usuario.append(usuario)
+    nombre = {
+        "sexo": "M","F"
+        "contraseña":"contraseña"
+
+    }
+nombre = input("Ingrese su nombre")
+sexo = input("Ingrese su nombre")
+Contraseña = input("Ingrese su contraseña")
+
+
+   
+    
+    
+
+def Buscar_usuario(buscar):
+    for in lista_usuario(buscar):
+       return
+       
+    
+
+def Eliminar_usuario(eliminar):
+    lista_usuario in eliminar
+    lista_usuario.remove(eliminar)
+    return
+
+  
+def menu(opcion,usuario):
+   print("1.Ingresar usuario")
+   print("2.Buscar usuario")
+   print("3.Elimnar usuario")
+   print("4.Salir")
+
+    opcion == input("Ingrese una opcion")
+   
+   if opcion == "1":
+    (Ingresar_usuario)(usuario)
+
+    if opcion == "2":
+       Buscar_usuario(usuario)
+
+    if opcion == "3":
+        (Eliminar_usuario)(usuario)
+
+    if opcion == "4":
+       print("Saliste del programa")
+    else:
+       print("Elija una opcion valida")
+    
+                                                            
+       
+
+
+
+    
+
+
+
+    
+
+
+
+
+Ingresar_usuario("oa")
