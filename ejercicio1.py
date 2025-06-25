d00d3c097de7106114ec47896667257423a7201f
elgatomaleante12 <12joaquinalvarado12@gmail.com>
2025-06-25 11:14:37 -0400

    avances_evaluacion42025

---
+++ ejercicio1.py
@@ -0,0 +1,40 @@
+lista_usuario = []
+
+def mostrar_menu():
+    print("BIENVENIDO A TU MENU PRINCIPAL")
+    print("Menu principal")
+    print("1.-Ingresar usuario")
+    print("2.-Buscar usuario.")
+    print("3.-Eliminar usuario")
+    print("4.-salir del programa")
+
+def ingresar_usuario(nombre_user, sexo, contraseña):
+    
+    
+
+    return
+
+    
+def buscar_usuario():
+    return
+
+
+def eliminar_usuario(nombre_usuario):
+    for usuario in lista_usuario:
+        if nombre_usuario in usuario:
+            lista_usuario.remove(usuario)
+            return True
+    return
+    
+
+def validar_contraseña():
+    return
+    
+
+def validar_sexo():
+    return
+
+
+def salir_programa():
+    return
+
