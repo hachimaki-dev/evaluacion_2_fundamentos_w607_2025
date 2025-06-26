45557d9a6b0a4d31e774e0eb27f8dd3c43e35943
axel <natandreik11@gmail.com>
2025-06-26 11:15:56 -0400

    evaluacion de axel levin

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,49 +1,57 @@
-lista_de_usuarios = []
 
 
+usuarios = {
+    "nombre_usuario" : "",
+    "sexo":"",
+    "contraseña":"",
+}
 
-name=[]
+def agregar_usu(usuarios):
+     print(usuarios)
 
-def agregar_usu(name, sexo, contraseña):
-        usuarios = {
-        name: {
-                "sexo": sexo,
-                "contraseña": contraseña
-            }
-        }
-        lista_de_usuarios.append(usuarios)
-        print(usuarios)
-        return
 
-def elimi(eliminar):
-      print(eliminar)
-      lista_de_usuarios.remove(eliminar)
+def elimi(usuarios):
+      print(usuarios)
       return
       
 
 while True:
-    print("---MENU---")
-    print("1-ingresar usuario")
-    print("2-buscar usuario")
-    print("3-eliminar usuario")
-    print("4-salir")
-    opcion = int(input(print("ingrese una opcion")))
+    while True:
+          try:
+            print("---MENU---")
+            print("1-ingresar usuario")
+            print("2-buscar usuario")
+            print("3-eliminar usuario")
+            print("4-salir")
+            opcion = int(input(print("ingrese una opcion")))
+            break
+          except:
+               print("ingrese un numero entero")
 
 
 
     if opcion == 1:
-        name = input("ingrese nombre de usuario:")
-        agregar_usu(name)
+        usuarios["nombre_usuario"] = input("ingrese nombre de usuario:")
+        usuarios["sexo"]=input("ingrese sexo:")
+        usuarios["contraseña"]=input("ingrese contraseña:")
+        agregar_usu(usuarios)
 
-
+ 
     if opcion == 2:
-            if lista_de_usuarios:
-                for i in lista_de_usuarios:
-                    print(lista_de_usuarios)
+                buscar = input("ingrese usuario a buscar:")
+                if buscar == usuarios["nombre_usuario"]:
+                    print("el sexo del usuario es",usuarios["sexo"], "y la contraseña es",usuarios["contraseña"])    
+                else:
+                    print("el usuario no se encuentra")
+                    
 
     if opcion == 3:
-        eliminar = input(print("ingrese usuario a eliminar"))
-        elimi(eliminar)
+        eliminar = input(print("ingrese usuario a eliminar:"))
+        if eliminar == usuarios["nombre_usuario"]:
+            del eliminar,usuarios["nombre_usuario"]
+            print("usuario eliminado con exito")
+        else:
+            print("no se pudo eliminar el usuario!")
 
     if opcion == 4:
            break
