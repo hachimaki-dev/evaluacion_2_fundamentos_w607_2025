3568d244a224b56c11ed676c5cfde86f087edbc6
Tzequiel <richardhernandezbarraza@gmail.com>
2025-06-26 13:58:26 -0400

    Evaluacion 4 finalizada - Richard Hernandez

--- ejercicio1.py
+++ ejercicio1.py
@@ -34,10 +34,12 @@ def filtro():
     while True:
         try:
             opcion=int(input("Ingrese una opción: \n"))
-            if opcion>=1 and opcion<=4:
+            if 1<=opcion<=4:
                 return opcion
+            else:
+                print("Debe ingresar un número entero del 1 al 4.")
         except ValueError:
-            print("Debe ingresar un número entero del 1 al 4")
+            print("Debe ingresar un número entero del 1 al 4.")
 
 def ingresar_usuario():
     nuevo_usuario={
@@ -53,62 +55,66 @@ def buscar_usuario(nombre_usuario):
     for usuarios in lista_usuarios:
         for nombre in usuarios:
             if nombre_usuario.lower()==nombre.lower():
-                print("Nombre de usuario encontrado")
+                print("Nombre de usuario encontrado.")
                 print("Datos del usuario: ", usuarios[nombre])
                 print("Volviendo al menu principal...")
+                return
+    print("Usuario no encontrado.")
 
 def eliminar_usuario(nombre_usuario):
     for usuarios in lista_usuarios:
         for nombre in usuarios:
             if nombre_usuario.lower()==nombre.lower():
-                print("Nombre de usuario encontrado")
+                print("Nombre de usuario encontrado.")
                 print("Eliminando usuario...")
                 lista_usuarios.remove(usuarios)
                 print("Usuario eliminado correctamente. Volviendo al menú principal...")
+                return
+    print("Usuario no encontrado.")
 
 def validar_usuario():
     while True:
-        try:
-            user=input("Ingrese el nombre de usuario: \n")
-            for usuarios in lista_usuarios:
-                if user in usuarios:
-                    print("Nombre de usuario invalido")  
-            print("Nombre de usuario valido.")
-            return user  
-        except:
-            print("El usuario ha sido ingresado anteriormente")
+        user=input("Ingrese el nombre de usuario: \n")
+        existe=False
+        for usuarios in lista_usuarios:
+            if user in usuarios:
+                existe=True
+                break
+        if existe:
+            print("Nombre de usuaio invalido.")
+        else:
+            print("Nombre de usuario válido.")
+            return user
 
 def validar_sexo():
     while True:
-        try:
-            sexo=input("Ingrese el sexo del usuario, use F para femenino y M para masculino: \n")
-            if sexo=="F" or sexo=="M":
-                print("El sexo se ha ingresado correctamente")
-                return sexo
-        except:
+        sexo=input("Ingrese el sexo del usuario, use F para femenino y M para masculino: \n")
+        if sexo=="F" or sexo=="M":
+            print("El sexo se ha ingresado correctamente.")
+            return sexo
+        else:
             print("Seleccione una opción valida, M o F solamente.")
 
 def validar_contraseña():
     while True:
-        try:
-            contraseña=input("Ingrese una contraseña: \n")
-            tiene_letras=False
-            tiene_numeros=False
-            tiene_espacios=False
-            if len(lista_usuarios)>=8:
-                for caracter in contraseña:
-                    if caracter.isalpha():
-                        tiene_letras=True
-                    if caracter.isdigit():
-                        tiene_numeros=True
-                    if caracter.isspace():
-                        tiene_espacios=True
-                if tiene_letras and tiene_numeros and tiene_espacios==False:
-                    print("Contraseña valida.")
-                    return contraseña
+        contraseña=input("Ingrese una contraseña: \n")
+        tiene_letras=False
+        tiene_numeros=False
+        tiene_espacios=False
+        if len(contraseña)>=8:
+            for caracter in contraseña:
+                if caracter.isalpha():
+                    tiene_letras=True
+                if caracter.isdigit():
+                    tiene_numeros=True
+                if caracter.isspace():
+                    tiene_espacios=True
+            if tiene_letras and tiene_numeros and tiene_espacios==False:
+                print("Contraseña valida.")
+                return contraseña
             else:
                 print("La contraseña no debe de tener espacios.")
-        except:
+        else:
             print("La contraseña no cumple con los requisitos. La contraseña necesita un minimo de 8 caracteres, sin espacios y tanto letras como números.")
 
 print("###   Sistema de gestión de usuarios   ###")
