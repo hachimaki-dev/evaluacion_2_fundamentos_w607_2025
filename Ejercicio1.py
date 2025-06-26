b380fc790860c3397e4aeca7940e64306b00391d
Lunalker <deinerquero1@gmail.com>
2025-06-26 11:12:02 -0400

    Evaluacion 4 jueves 26

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -18,7 +18,8 @@ def ingresar_usuario(nombre_usuario, sexo, contraseña):
                 print("El usuario ya existe")
             else:
                 lista_usuario.append(usuario)
-        print("La contraseña no cumple con lo requerido")
+        else:
+            print("La contraseña no cumple con lo requerido")
     
     return
 
@@ -58,27 +59,44 @@ def validar_usuario(nombre_usuario):
     return False
 
 def mostrar_menu():
-    print("MENU PRINCIPAL")
-    print("1.- Ingresar usuario.")
-    print("2.- Buscar usuario.")
-    print("3.- Eliminar usuario.")
-    print("4.- Salir.")
     while True:
+        print("MENU PRINCIPAL")
+        print("1.- Ingresar usuario.")
+        print("2.- Buscar usuario.")
+        print("3.- Eliminar usuario.")
+        print("4.- Salir.")
         try:
             opcion_elegida = int(input("Ingresa que desea hacer"))
+            if opcion_elegida >= 5:
+                return
             break
         except:
             print("por favor ingrese una opcion valida ")
     while True:
         if opcion_elegida == 1:
-            nombre = input("Ingrese el nombre de usuario").lower().upper()
+            nombre = input("Ingrese el nombre de usuario")
             sexo = input("Ingrese el sexo del usuario ingresado (M/F)")
             contraseña = input("Ingrese la contraseña del usuario")
             ingresar_usuario(nombre, sexo, contraseña)
+            return
+        elif opcion_elegida == 2:
+            nombre = input("Ingrese el nombre de usuario que desea buscar ")
+            buscar_usuario(nombre)
+        elif opcion_elegida == 3:
+            nombre = input("Ingresa el usuario que desea eliminar ")
+            eliminar_usuario(nombre)
             
-    return
+            
+        elif opcion_elegida == 4:
+            print("Saliendo del menu")
+            
+            
+        return
 
-print(mostrar_menu())
+mostrar_menu()
+
+ingresar_usuario("Deiner", "M", "DEINER123345")
+buscar_usuario("deiner")
 
 
 
